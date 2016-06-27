7984be4cbbbd72982ca7fb962e4f113bdac6a6cb
Hady Rashwan <h2rashwan@gmail.com>
2016-06-27 21:18:34 +0200

    colors and both what msgs

--- public/js/app.js
+++ public/js/app.js
@@ -5,9 +5,10 @@
 	})
 
 	socket.on('toALL',function (msg) {
+
 				console.log('msg here '+msg.text)
 				var temp = document.getElementById("msgs").innerHTML
-				document.getElementById("msgs").innerHTML = temp+ "<br>" + msg.text ;
+				document.getElementById("msgs").innerHTML = temp+ "<br>" +"<font color='red'>"+msg.text+"</font>" ;
 	})
 
 var  $form = jQuery("#message-form")
@@ -16,6 +17,8 @@ $form.on('submit',function (event) {
 	event.preventDefault()
 	var msg={}
 	 msg.text=	$form.find("input[name=message]").val()
+	 			var temp = document.getElementById("msgs").innerHTML
+				document.getElementById("msgs").innerHTML = temp+ "<br>" +"<font color='blue'>"+msg.text+"</font>" ;
 socket.emit('msg',msg)
 $form.find("input[name=message]").val("")
 })
