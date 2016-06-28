f9f2160c9534b024ebe7d528a7f6c7c0a096cc53
Hady Rashwan <h2rashwan@gmail.com>
2016-06-28 12:07:09 +0200

    rooms are added conversation are be taken to the logs

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,29 @@
 	var person = prompt("Please enter your name", "Harry Potter");
+	roomName = prompt("Please enter the room name or create a new one press enter if u want the defualt", "toALL");
 	var socket = io();
-	socket.on('connect',function () {
-		// body...
-		console.log("the user has entered ");
+	socket.on('connect', function() {
+	    // body...
+	    console.log("the user has entered ");
 	})
 
-	socket.on('toALL',function (msg) {
+	socket.on(roomName, function(msg) {
 
-				console.log('msg here '+msg.text)
-				var temp = document.getElementById("msgs").innerHTML
-				document.getElementById("msgs").innerHTML = temp+ "<br>" +"<font color='red'>"+msg.text+" @ "+ moment().format('LTS')+"</font>" ;
+	    console.log('msg here ' + msg.text)
+	    var temp = document.getElementById("msgs").innerHTML
+	    document.getElementById("msgs").innerHTML = temp + "<br>" + "<font color='red'>" + msg.text + " @ " + moment().format('LTS') + "</font>";
 	})
 
-var  $form = jQuery("#message-form")
-$form.on('submit',function (event) {
-	console.log('hello')
-	event.preventDefault()
-	var msg={}
-	 msg.text=$form.find("input[name=message]").val()+" -- " +person
-	 			var temp = document.getElementById("msgs").innerHTML
-				document.getElementById("msgs").innerHTML = temp+ "<br>" +"<font color='blue'>"+msg.text+" @ "+ moment().format('LTS')+"</font>" ;
-socket.emit('msg',msg)
-$form.find("input[name=message]").val("")
-})
+	var $form = jQuery("#message-form")
+	$form.on('submit', function(event) {
+	    console.log('hello')
+	    event.preventDefault()
+	    var msg = {}
+	    msg.room = roomName
+	    msg.time =
+	        msg.person = person
+	    msg.text = $form.find("input[name=message]").val() + " -- " + person
+	    var temp = document.getElementById("msgs").innerHTML
+	    document.getElementById("msgs").innerHTML = temp + "<br>" + "<font color='blue'>" + msg.text + " @ " + moment().format('LTS') + "</font>";
+	    socket.emit('msg', msg)
+	    $form.find("input[name=message]").val("")
+	})
--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 var PORT = process.env.PORT || 3000
 var moment = require("moment")
-var  now = moment();
+var now = moment();
 var express = require('express')
 var app = express()
-var http =require('http').Server(app);
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname + '/public'))
 
 
-io.on('connection',function(socket){
-	console.log('User connceted via socket.io');
+io.on('connection', function(socket) {
+    console.log('User connceted via socket.io');
 
-	socket.on('msg',function (msg) {
-		// body...
-		console.log('msg : '+msg.text)
-		socket.broadcast.emit('toALL',msg)
-	})
+    socket.on('msg', function(msg) {
+        // body...
+        console.log('msg : ' + msg.text + " at time " + msg.time + " in room : " + msg.room)
+        socket.broadcast.emit(msg.room, msg)
+    })
 
-	socket.emit('msg',{ // not being sent for the moment
-		text:'hello to the chat app'
-	})
+    socket.emit('msg', { // not being sent for the moment
+        text: 'hello to the chat app'
+    })
 
 })
 
 
 
-http.listen(PORT, function () {
-	// body...
-	console.log(" Server is now running on "+PORT); 
+http.listen(PORT, function() {
+    // body...
+    console.log(" Server is now running on " + PORT);
 })
